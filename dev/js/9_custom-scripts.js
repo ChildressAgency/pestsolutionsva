2bfdd015f6721635d87187ab9876b6848cf59be5
childressdev <jcampbell@childressagency.com>
2020-02-20 16:38:27 -0500

    added hero slide animation

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -4,4 +4,24 @@
 
 jQuery(document).ready(function($){
   AOS.init();
+
+  //var $hero = $('.hp-hero');
+  var $hero = $('[data-hero_images]');
+
+  if($hero.length > 0){
+    var heroSlides = $hero.data('hero_images');
+    var numSlides = heroSlides.length;
+    var slideIndex = 0;
+    var slideShow = function(){
+      if(slideIndex >= numSlides){
+        slideIndex = 0;
+      }
+
+      //console.log(heroSlides[slideIndex]);
+      $hero.css('background-image', 'url("' + heroSlides[slideIndex] + '")').show(0, function(){ setTimeout(slideShow, 5000); });
+      slideIndex++;
+    };
+
+    slideShow();
+  }
 });
