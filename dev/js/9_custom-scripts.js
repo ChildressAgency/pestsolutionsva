9a172c705af4149f4217336a7ab2cbeae87b396c
childressdev <jcampbell@childressagency.com>
2020-02-27 11:41:05 -0500

    added normalize carousel heights

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -56,4 +56,43 @@ jQuery(document).ready(function($){
       $($imgEl).attr('src', img).fadeIn();
     });
   });
-});
+
+  $('#did-you-know-carousel .carousel-inner .carousel-item').carouselHeights();
+}); //end jquery
+
+/**
+ * Normalize Carousel Heights
+ */
+$.fn.carouselHeights = function () {
+  var items = $(this), //grab all slides
+    heights = [], //create empty array to store height values
+    tallest; //create variable to make note of the tallest slide
+
+  var normalizeHeights = function () {
+    items.each(function () { //add heights to array
+      heights.push($(this).height());
+    });
+    tallest = Math.max.apply(null, heights); //cache largest value
+    if (tallest < 300) { tallest = 300; }
+    items.each(function () {
+      $(this).css('min-height', tallest + 'px');
+    });
+  };
+
+  normalizeHeights();
+
+  $(window).on('resize orientationchange', function () {
+    //reset vars
+    tallest = 0;
+    heights.length = 0;
+
+    items.each(function () {
+      $(this).css('min-height', '0'); //reset min-height
+    });
+    normalizeHeights(); //run it again 
+  });
+
+  $('.locations-map').each(function () {
+    map = new_map($(this));
+  });
+};
