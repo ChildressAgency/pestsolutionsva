c2aa88b114cc1233f41293d682817589072b9b8e
childressdev <jcampbell@childressagency.com>
2020-03-05 17:38:28 -0500

    separate fields for title and caption

--- wp-theme-files/partials/blocks/image_title_description.php
+++ wp-theme-files/partials/blocks/image_title_description.php
@@ -1,7 +1,7 @@
 <?php $block_image = get_field('block_image'); ?>
 
 <div class="img-title-desc">
-  <h4><?php echo esc_html($block_image['title']); ?></h4>
+  <h4><?php the_field('image_title'); ?></h4>
   <img src="<?php echo esc_url($block_image['url']); ?>" class="img-fluid d-block mx-auto" alt="<?php echo esc_attr($block_image['alt']); ?>" />
-  <p><?php echo esc_html($block_image['caption']); ?></p>
+  <p><?php the_field('image_description') ?></p>
 </div>
