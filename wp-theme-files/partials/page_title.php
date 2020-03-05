7fac4e93f521c0651636106ce96825fe446f7b2f
childressdev <jcampbell@childressagency.com>
2020-03-05 14:50:53 -0500

    dont show regular title

--- wp-theme-files/partials/page_title.php
+++ wp-theme-files/partials/page_title.php
@@ -1,2 +1,5 @@
-<?php $page_title = get_field('page_title'); ?>
-<h1 class="page-title"><?php echo $page_title ? esc_html($page_title) : get_the_title(); ?></h1>
+<?php
+$page_title = get_field('page_title');
+if($page_title){
+  echo '<h1 class="page-title">' . wp_kses_post($page_title) . '</h1>';
+}
