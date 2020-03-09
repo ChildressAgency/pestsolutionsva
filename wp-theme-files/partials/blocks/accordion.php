b9101640443b60c4c6f4a509bdb9907456c0a790
childressdev <jcampbell@childressagency.com>
2020-03-09 11:12:00 -0400

    fix typos

--- wp-theme-files/partials/blocks/accordion.php
+++ wp-theme-files/partials/blocks/accordion.php
@@ -4,13 +4,13 @@
     <div class="card">
       <div id="accordion-title-<?php echo $i; ?>" class="card-header">
         <h4>
-          <button class="open collapsed" data-toggle="collapse" data-target="#accordion-content-<?php echo $i; ?>" aria-expanded="false" aria-controls="accordion-content-<?php echo $i; ?>"><?php the_sub_field('accordion_title'); ?></button>
+          <button class="opener collapsed" data-toggle="collapse" data-target="#accordion-content-<?php echo $i; ?>" aria-expanded="false" aria-controls="accordion-content-<?php echo $i; ?>"><?php the_sub_field('accordion_title'); ?></button>
         </h4>
       </div>
       <div id="accordion-content-<?php echo $i; ?>" class="collapse" aria-labelledby="accordion-title-<?php echo $i; ?>" data-parent="#block-accordion">
         <div class="card-body">
           <div class="accordion-panel">
-            <?php the_sub_field('accordion_title'); ?>
+            <h3><?php the_sub_field('accordion_title'); ?></h3>
             <div class="accordion-panel-body">
               <?php the_sub_field('accordion_content'); ?>
             </div>
